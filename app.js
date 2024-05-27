724a757ca22a36f00b6753a2356c9f4d2700ae84
Justudent <justudent09@mail.ru>
2024-05-27 18:48:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
     divs.forEach(div => {
         div.addEventListener('click', function() {
-            // Удаление класса 'active' со всех div
-            divs.forEach(d => d.classList.remove('active'));
-            // Добавление класса 'active' на нажатый div
-            this.classList.add('active');
+            divs.forEach(d => {
+                d.classList.remove('active'); // Удаление класса 'active' со всех div
+                d.querySelector('.circle').style.background = '#28272C'; // Сброс цвета фона у всех circle
+                d.style.color = '#5D5C61'; // Сброс цвета текста у всех my-div
+            });
+            this.classList.add('active'); // Добавление класса 'active' на нажатый div
+            this.querySelector('.circle').style.background = 'linear-gradient(#B4B2E2, #9293DF)'; // Изменение цвета фона у circle на нажатом div
+            this.style.color = 'white'; // Изменение цвета текста на нажатом div
         });
     });
 });
