f6b338f7432021e1baebda9ab5d688add4ec12e0
Justudent <justudent09@mail.ru>
2024-05-28 09:41:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,3 +14,14 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`;
+
+usercard.appendChild(p);
