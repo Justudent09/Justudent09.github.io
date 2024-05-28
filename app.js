c16072f82e0965ce3fac52e3dd65b0401ace7fa1
Justudent <justudent09@mail.ru>
2024-05-28 09:51:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,15 +15,3 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-
-let tg = window.Telegram.WebApp;
-
-
-let usercard = document.getElementById("usercard");
-
-let p = document.createElement("p");
-
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
-
-usercard.appendChild(p);
