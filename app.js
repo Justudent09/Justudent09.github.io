99518cb70a725414afda3bd36d091e29791159d5
Justudent <justudent09@mail.ru>
2024-05-29 10:28:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -40,3 +40,37 @@ const now = new Date();
 
         document.getElementById("currentDateDiv").innerText = currentDate;
       
+
+const daysInMonth = new Date(year, now.getMonth() + 1, 0).getDate();
+    const daysContainer = document.getElementById("daysContainer");
+
+    const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    for (let i = 1; i <= daysInMonth; i++) {
+        const dayDiv = document.createElement("div");
+        dayDiv.classList.add("my-div");
+
+        const circleDiv = document.createElement("div");
+        circleDiv.classList.add("circle");
+        circleDiv.textContent = i;
+
+        const dayName = document.createElement("p");
+        const date = new Date(year, now.getMonth(), i);
+        dayName.textContent = dayNames[date.getDay()];
+
+        dayDiv.appendChild(circleDiv);
+        dayDiv.appendChild(dayName);
+        daysContainer.appendChild(dayDiv);
+
+        
+        dayDiv.addEventListener('click', function() {
+            document.querySelectorAll('.my-div').forEach(d => {
+                d.classList.remove('active');
+                d.querySelector('.circle').style.background = '#28272C';
+                d.style.color = '#5D5C61';
+            });
+            this.classList.add('active');
+            this.querySelector('.circle').style.background = 'linear-gradient(#B4B2E2, #9293DF)';
+            this.style.color = 'white';
+        });
+    }
