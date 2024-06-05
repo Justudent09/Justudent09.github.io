ddc8cb451c02ce8ccdffea488b17f0f153d82696
Justudent <justudent09@mail.ru>
2024-06-05 12:02:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,13 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     if (!schedule) {
-        document.body.innerHTML = '<div id="centeredText"><h1 style="color: white;">ВЫ НЕ СТУДЕНТ ФИЛИАЛА МГУ В ГРОЗНОМ</h1></div>';
+        document.body.innerHTML = '<div id="centeredText"><div style="
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background-color: #28272C;
+    border-radius: 25px;"><h1 style="color: white;">ВЫ НЕ СТУДЕНТ ФИЛИАЛА МГУ В ГРОЗНОМ</h1></div></div>';
         return;
     }
 
